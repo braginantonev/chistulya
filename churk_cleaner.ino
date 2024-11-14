fb6f267a01f37516cad75849b8961df4dce72beb
San4hi <antonsayfermay@gmail.com>
2024-11-14 22:24:56 +0300

    add servos initialize

--- churk_cleaner.ino
+++ churk_cleaner.ino
@@ -7,12 +7,22 @@ AF_DCMotor rightMotor(2);
 AF_DCMotor brushMotor(3);
 AF_DCMotor* pmotors[3] = {&leftMotor, &rightMotor, &brushMotor};
 
+//--- Servos ---//
+Servo leftServo;
+Servo rightServo;
+Servo batteryServo;
+Servo* pservos[3] = {&leftServo, &rightServo, &batteryServo};
+
 void setup() {
   //Initialize motors
   for (byte i = 0; i < 3; i++) {
     (*pmotors[i]).setSpeed(200);
     (*pmotors[i]).run(RELEASE);
   }
+
+  //Initialize servos
+  for (byte i = 0; i < 3; i++)
+    (*pservos[i]).attach(i+3); //3 4 5
 }
 
 void loop() {
