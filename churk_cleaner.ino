5058cbd374a13ff50fab697577c5d5b67e39fa47
San4hi <antonsayfermay@gmail.com>
2024-11-14 22:17:10 +0300

    add motors initialize

--- churk_cleaner.ino
+++ churk_cleaner.ino
@@ -1,8 +1,18 @@
+#include <Servo.h>
+#include <AFMotor.h>
 
+//--- Motors ---//
+AF_DCMotor leftMotor(1);
+AF_DCMotor rightMotor(2);
+AF_DCMotor brushMotor(3);
+AF_DCMotor* pmotors[3] = {&leftMotor, &rightMotor, &brushMotor};
 
 void setup() {
-  // put your setup code here, to run once:
-
+  //Initialize motors
+  for (byte i = 0; i < 3; i++) {
+    (*pmotors[i]).setSpeed(200);
+    (*pmotors[i]).run(RELEASE);
+  }
 }
 
 void loop() {
