833faa5b7f91eb0fe4a1900fedbd757a9dba5027
San4hi <antonsayfermay@gmail.com>
2024-11-22 23:41:55 +0300

    add missing commands

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -5,10 +5,16 @@ using namespace modules;
 using namespace scomm;
 
 void (*pcommands[])(int[]) = {
-  motors::runMotor,
-  motors::goForward,
-  motors::stopMotors,
-  motors::stopAllMotors
+  motors::runMotor,           //1
+  motors::goForward,          //2
+  motors::goRound,            //3
+  motors::stopMotors,         //4
+  motors::stopBrush,          //5
+  motors::stopAllMotors,      //6
+  nullptr,                    //7 - reserved
+  nullptr,                    //8 - reserved
+  nullptr,                    //9 - reserved
+  servos::rotateServo         //10
 };
 
 void setup() {
