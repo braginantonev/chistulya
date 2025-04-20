f034310a47217065c5dade56f71e9d4bae3de5d3
San4hi <antonsayfermay@gmail.com>
2025-04-20 19:33:08 +0300

    add command handler

--- raspberry/main.py
+++ raspberry/main.py
@@ -1,101 +1,35 @@
 import libs.serialPrint as serp
-import libs.redCam as cam
-import libs.sonars as son
+#import libs.redCam as cam
+#import libs.sonars as son
+import libs.server as server
 import time as tm
+import threading
 
 SPEED = 100
 
-#! Расстояние временное, должно получаться с сервера
-STREET_LENGTH_CM = 1000
+#TODO: Доделать алгоритм уборки
+def start_cleaning(street_length: float): 
+    pass
 
-son.init_sonars()
+#TODO: Добавить описание 15 команды в доки
+def run_command(command: str):
+    if command[:2] == "15":
+        start_cleaning(0) #TODO: Заменить на значение из первого параметра
+    else:
+        serp.send_command(command)
+
+server_thread = threading.Thread(target=server.run)
+server_thread.start()
+
 serp.showInfo()
 
-def forward(len):
-    serp.run_command(f'010{SPEED}00000000')
-    tm.sleep(len / SPEED)
-
-
-def round_right():
-    global ROUND_MOVE_TIME
-    serp.run_command(f"0200000{SPEED}0000")
-    tm.sleep(ROUND_MOVE_TIME)
-
-
-def round_left():
-    serp.run_command(f"020{SPEED}00000000")
-    tm.sleep(ROUND_MOVE_TIME)
-
-
-def check_len_to_left_border():
-    if son.get_distances()[0] >= 15:
-        round_left()
-        serp.run_command(f'010{SPEED}00000000')
-        round_left()
-        forward(STREET_LENGTH_CM)
-        round_right()
-        serp.run_command(f'010{SPEED}00000000')
-        round_right()
-        return True
-    else:
-        round_left()
-        round_left()
-        forward(STREET_LENGTH_CM)
-        round_left()
-        round_left()
-        return False
+while (True):
+    command = server.get_command() # Command, time
+    if command == None:
+        tm.sleep(1)
+        continue
     
-def check_len_to_right_border():
-    if son.get_distances()[1] >= 15:
-        round_right()
-        serp.run_command(f'010{SPEED}00000000')
-        round_right()
-        forward(STREET_LENGTH_CM)
-        round_left()
-        serp.run_command(f'010{SPEED}00000000')
-        round_left()
-        return True
-    else:
-        round_left()
-        round_left()
-        forward(STREET_LENGTH_CM)
-        round_left()
-        round_left()
-        return False
-
-
-def find_center():
-    distances = son.get_distances()
-    if distances[0] != distances[1]:
-        move_len = abs(distances[0] - distances[1]) / 2
-        ROUND_MOVE_TIME = move_len / SPEED
-        if distances[0] < distances[1]:
-            round_left()
-            serp.run_command(f'010{SPEED}00000000')
-            tm.sleep(ROUND_MOVE_TIME) #TODO: Заменить на расстояние / скорость 
-            round_right()
-        else:
-            round_right()
-            serp.run_command(f'010{SPEED}00000000')
-            tm.sleep(ROUND_MOVE_TIME) #TODO: Заменить на расстояние / скорость
-            round_left()
-
-
-while True:
-    find_center()
-    
-    while check_len_to_left_border:
-        forward(STREET_LENGTH_CM)
-        
-        check_len_to_left_border()
-
-    while check_len_to_right_border:
-        forward(STREET_LENGTH_CM)
-        
-        check_len_to_left_border()
-    break
-
-
-
-
-
+    run_command(command[0])
+    tm.sleep(command[1])
+    server.complete_command()
+    tm.sleep(0.5) # Delay
